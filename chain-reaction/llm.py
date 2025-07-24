27d07d0b2b3a4e14c7aa49da6fcdc0832a0cef21
Siddhartha Gunti <siddhartha.gunti191@gmail.com>
2025-07-24 15:13:08 +0800

    1. Slight reorg. 2. Adding Gemini since mistral is 5xxing 3. Adding llm rotation

--- chain-reaction/llm.py
+++ chain-reaction/llm.py
@@ -75,3 +75,88 @@ class Mistral(LLM):
         
         return response.json()["choices"][0]["message"]["content"]
     
+
+    
+class Gemini(LLM):
+
+    models = {
+        "gemini-2.0-flash": {
+            "supportsImages": True,
+        }
+    }
+
+    def __init__(self, api_key: str):
+        super().__init__(api_key)
+
+    def format_messages(self, model, messages: list[dict]):
+        # each message is of the form role, content and type. content = {"type": "image_url", "dataUrl": "url", "mime": "image/png"} for images. type = image_url and text other wise
+        formatted_messages = []
+        for message in messages:
+            # strip away system messsages if any
+            if message["role"] == "system":
+                continue
+            if message["type"] == "image_url" and self.models[model]["supportsImages"]:
+                formatted_messages.append({
+                    "role": "user" if message["role"] == "user" else "model",
+                    "parts": [
+                        {
+                            "inlineData": {
+                                "data": message["content"]["dataUrl"].split(",")[1],
+                                "mime_type": message["content"]["mime"]
+                            }
+                        }
+                    ]
+                })
+            else:
+                formatted_messages.append({
+                    "role": "user" if message["role"] == "user" else "model",
+                    "parts": [
+                        {
+                            "text": message["content"]
+                        }
+                    ]
+                })
+
+        return formatted_messages
+
+    def generate_text(self, model, messages: list[dict], max_tokens: int = 1000):
+        if model not in self.models:
+            raise Exception(f"Model {model} not supported")
+
+        formatted_messages = self.format_messages(model, messages)
+
+        system_messages = [msg for msg in formatted_messages if msg["role"] == "system"]
+        system_message = system_messages[0]["content"][0]["text"] if len(system_messages) > 0 and "content" in system_messages[0] and system_messages[0]["type"] == "text" else ""
+
+        body = {
+            "model": model,
+            "contents": formatted_messages,
+            "generationConfig": {
+                "temperature": 1.0,
+                "topP": 0.8,
+                "topK": 10,
+                "maxOutputTokens": max_tokens
+            },
+            "systemInstruction": {
+                "parts": [
+                    {
+                        "text": system_message
+                    }
+                ]
+            }
+        }
+
+        response = requests.post(
+            "https://generativelanguage.googleapis.com/v1beta/models/" + model + ":generateContent?key=" + self.api_key,
+            json=body,
+            headers={
+                "Content-Type": "application/json",
+                "x-goog-api-key": self.api_key
+            }
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to generate text: {response.status_code} {response.text}")
+        
+        return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+    
